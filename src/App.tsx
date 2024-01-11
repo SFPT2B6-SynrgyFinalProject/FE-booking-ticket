989b1a01c8c51b0e85d2d194fc4c9961f802f9b1
alkmn <alkmn99@gmail.com>
2024-01-11 15:13:01 +0700

    refactor: update login integration with BE API

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,37 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import PrivateProvider from "./providers/PrivateProvider";
+import PublicProvider from "./providers/PublicProvider";
+import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Home/>
-    },
-    {
-        path: '/login',
-        element: <Login/>
-    },
-    {
-        path: '/register',
-        element: <Register/>
-    }
+	{
+		element: <PrivateProvider/>,
+		children: [
+			{
+				path: "/",
+				element: <Home/>
+			}
+		]
+	},
+	{
+		element: <PublicProvider/>,
+		children: [
+			{
+				path: "/login",
+				element: <Login/>
+			},
+			{
+				path: "/register",
+				element: <Register/>
+			}
+		]
+	},
+	{
+		path: "*",
+		element: <NotFound/>
+	}
 ]);
 
 export default function App () {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import { NavigateFunction, useNavigate } from "react-router-dom";
+
 export default function Home() {
-    return (
-        <div>
-            <h1 className="text-3xl text-center font-semibold">Hello world!</h1>
-        </div>
-    )
+  const navigate: NavigateFunction = useNavigate()
+  const handleLogout = function () {
+    localStorage.clear();
+    navigate("/login");
+  }  
+  return (
+    <div>
+        <h1 className="text-3xl text-center font-semibold">Hello world!</h1>
+        <button onClick={handleLogout}>Logout</button>
+    </div>
+  )
 }
