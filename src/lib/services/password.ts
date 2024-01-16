091c568f51795846da76d76c7de159872a3e9031
Yusuf  Maruf <achmadusufalmaruf@gmail.com>
2024-01-16 11:07:28 +0700

    send resetpassword link

---
+++ src/lib/services/password.ts
@@ -0,0 +1,23 @@
+import { fetchInstance } from "./core";
+
+export type ForgotEmailRequestBody = {
+    email: string;
+};
+
+export type ForgotPasswordResponseBody = {
+    data?: {
+        email: string;
+    };
+    message: string;
+    status: string;
+};
+
+export async function SendPasswordResetLink(
+    form: ForgotEmailRequestBody
+): Promise<ForgotPasswordResponseBody> {
+    return await fetchInstance({
+        endpoint: "/api/forget-password",
+        method: "POST",
+        data: form,
+    });
+}
