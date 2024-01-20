806b076b90a8119873a658648270738123406899
aldilukmn <aldilukman99@gmail.com>
2024-01-20 17:03:28 +0700

    feat(component): add navbar component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { NavigateFunction, useNavigate } from "react-router-dom";
 import MyComponent from "../components/Doc";
-import Footer from "../components/footer";
 
 export default function Home() {
   const navigate: NavigateFunction = useNavigate()
@@ -16,7 +15,6 @@ export default function Home() {
         <h1 className="text-3xl text-center font-semibold">Hello world!</h1>
         <button onClick={handleLogout}>Logout</button>
       <MyComponent />
-      <Footer />
     </div>
   )
 }
