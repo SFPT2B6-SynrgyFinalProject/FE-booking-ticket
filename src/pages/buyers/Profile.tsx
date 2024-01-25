8c6d1000509cf9e9b74c0e1f5a573ebbf5786b62
agung749 <agungciccone749@gmail.com>
2024-01-26 05:41:21 +0700

    oke

--- src/pages/buyers/Profile.tsx
+++ src/pages/buyers/Profile.tsx
@@ -3,6 +3,8 @@ import { Icon } from '@iconify/react/dist/iconify.js';
 import { Outlet, useLocation } from 'react-router';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../config/redux/store';
+import { Card } from '../../components/Card';
+import { ContainerPage } from '../../components/common-page/ContainerPage';
 function Profile() {
   const location = useLocation();
     const[active,setActive] = useState<number>(1);
@@ -18,12 +20,12 @@ function Profile() {
   }, [location.pathname]);
 
   return (
-    <div className="justify-center items-center block lg:h-full bg-white mt-3">
+<ContainerPage>
       <div className="container mx-auto md:pt-16 md:pl-16 lg:pt-5 flex flex-col lg:flex-row">
         {/* Profile Image Section */}
-        <div className="shadow-xl md:w-[400px] rounded-xl ml-6 lg:mr-4 h-[300px] p-5 mr-6 mb-8">
-          <div className='inline-flex mb-8'>
-            <div className='flex items-center justify-center bg-slate-100 ml-6 p-2 h-20 w-20 rounded-full'>
+        <Card customStyle='mr-5 h-1/3 md:w-1/3 mb-8 sm:w-full'>
+          <div className='inline-flex mb-4 '>
+            <div className='flex items-center justify-center bg-slate-100  h-20 w-20 rounded-full'>
               <Icon
                 width={30}
                 height={30}
@@ -38,28 +40,30 @@ function Profile() {
             </div>
           </div>
           <a href='/profile/'><div className='text-black inline-flex w-[100%] pl-5 '>
-            <Icon icon={"uil:setting"} className={active === 1 ? "text-blue-900 mt-1 ml-8" : "text-black mt-1 ml-8"} ></Icon>
+            <Icon icon={"uil:setting"} className={active === 1 ? "text-blue-900 mt-1 " : "text-black mt-1 "} ></Icon>
             <h5 className={active === 1 ? "text-blue-900 ml-5 " : "text-black ml-5 "}> Informasi Akun </h5>
           </div></a>
           <a href='/profile/reset'>
             <div className='text-black inline-flex w-[100%] mt-2 pl-5'>
-              <Icon icon={"mingcute:eye-line"} className={active === 2 ? "text-blue-900 mt-1 ml-8" : "text-black mt-1 ml-8"} ></Icon>
+              <Icon icon={"mingcute:eye-line"} className={active === 2 ? "text-blue-900 mt-1 " : "text-black mt-1 "} ></Icon>
               <h5 className={active === 2 ? "text-blue-900 ml-5 " : "text-black ml-5 "}> Password </h5>
             </div>
           </a>
           <div className='text-black inline-flex w-[100%] mt-2 pl-5'>
-            <Icon icon={"icons8:shutdown"} className={active === 3 ? "text-blue-900 mt-1 ml-8" : "text-black mt-1 ml-8"} ></Icon>
+            <Icon icon={"icons8:shutdown"} className={active === 3 ? "text-blue-900 mt-1 " : "text-black mt-1 "} ></Icon>
             <h5 className={active === 3 ? "text-blue-900 ml-5 " : "text-black ml-5 "}> Log Out </h5>
           </div>
-        </div>
+          </Card>
 
         {/* Information Account Section */}
-        <div className='shadow-xl rounded-xl md:h-[700px] p-8 md:w-full lg:w-3/4 mb-8'>
+        <Card  customStyle="md:pt-10 w-full mb-4 p-10" >
+        
           <Outlet  ></Outlet>
-        </div>
+        
+        </Card>
       </div>
       {/* Footer */}
-    </div>
+      </ContainerPage>
   );
 }
 
