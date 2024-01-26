b2448fde5b3d1118e0de0db01b7b652ebe336064
Karim Hasibuan <amfibicoding19@gmail.com>
2024-01-27 00:27:45 +0700

    refactor: adjust to dynamic data.

--- src/components/Tablev2.tsx
+++ src/components/Tablev2.tsx
@@ -11,125 +11,19 @@ interface Row {
   role: string;
 }
 
-const handleEdit = (id: number) => {
-  // Handle edit logic here
-  console.log(`Editing record with ID ${id}`);
-};
+interface TableColumn {
+  name: any;
+  width?: any;
+  selector?: (row: Row) => string;
+  sortable?: any;
+}
 
-const handleDelete = (id: number) => {
-  // Handle delete logic here
-  console.log(`Deleting record with ID ${id}`);
-};
+interface TableProps {
+  columns: TableColumn[];
+  data: Row[];
+}
 
-const columns = [
-  {
-    name: "Name",
-    width: '12rem',
-    selector: (row: Row) => row.name,
-    sortable: true,
-  },
-  {
-    name: "Email",
-    width: '13rem',
-    selector: (row: Row) => row.email,
-    sortable: true,
-  },
-  {
-    name: "Nomor Telepon",
-    width: '10rem',
-    selector: (row: Row) => row.nohp,
-    sortable: true,
-  },
-  {
-    name: "Role",
-    width: '8rem',
-    selector: (row: Row) => row.role,
-    sortable: true,
-  },
-  {
-    name: "Action",
-    width: 'max-content',
-    center: true,
-    cell: (row: Row) => (
-      <div className="flex items-center py-2">
-        <Button
-          onClick={() => handleEdit(row.id)} // Assuming you have a delete function
-          className={`mr-2 text-sm font-bold bg-blue-900 text-white rounded-lg`} size="xs"
-        >
-          Edit <Icon icon="bx:edit" width={20} className="text-white" />
-        </Button>
-
-        <Button
-          onClick={() => handleDelete(row.id)} // Assuming you have a delete function
-          className={`text-sm font-bold bg-red-600 text-white rounded-lg`} size="xs"
-        >
-          Hapus <Icon icon="uil:trash" width={20} className="text-white" />
-        </Button>
-      </div>
-    ),
-  },
-];
-
-const data = [
-  {
-    id: 1,
-    name: "Admin",
-    nohp: "081234567890",
-    email: "Admin@gmail.com",
-    role: "Admin",
-  },
-  {
-    id: 2,
-    name: "Pengguna",
-    nohp: "081234567890",
-    email: "Pengguna@gmail.com",
-    role: "Pembeli",
-  },
-  {
-    id: 3,
-    name: "Pengguna",
-    nohp: "081234567890",
-    email: "Pengguna@gmail.com",
-    role: "Pembeli",
-  },
-  {
-    id: 4,
-    name: "Pengguna",
-    nohp: "081234567890",
-    email: "Pengguna@gmail.com",
-    role: "Pembeli",
-  },
-  {
-    id: 5,
-    name: "Pengguna",
-    nohp: "081234567890",
-    email: "Pengguna@gmail.com",
-    role: "Pembeli",
-  },
-];
-
-const customStyles = {
-  rows: {
-    style: {
-      fontSize: "16px",
-      // minWidth: "99px",
-    },
-  },
-  headRow: {
-    style: {
-      fontSize: "16px",
-      background: "#F0F0F0",
-    },
-  },
-  headCells: {
-    style: {},
-  },
-  cells: {
-    style: {},
-  },
-};
-
-export const Tablev2: React.FC = () => {
+const Tablev2: React.FC<TableProps> = ({ columns, data }) => {
   const [records, setRecords] = useState(data);
 
   function handleFilter(e: React.ChangeEvent<HTMLInputElement>) {
@@ -142,9 +36,23 @@ export const Tablev2: React.FC = () => {
     setRecords(newData);
   }
 
+  const customStyles = {
+    rows: {
+      style: {
+        fontSize: "16px",
+      },
+    },
+    headRow: {
+      style: {
+        fontSize: "16px",
+        background: "#F0F0F0",
+      },
+    },
+  };
+
   return (
-    <div className="w-4/6 mx-auto my-6">
-      <div className="border border-gray-300 p-4 rounded-xl">
+    <div className="w-full mx-auto my-6">
+      <div className="border-gray-300 p-4 rounded-xl">
         <div className="flex justify-between items-center mt-3 mb-10">
           <Button className={`bg-green-700 text-white rounded-lg`} size="sm">
             Tambah Pengguna <Icon icon="prime:file-import" width={20} className="text-white" />
@@ -163,7 +71,6 @@ export const Tablev2: React.FC = () => {
           data={records}
           customStyles={customStyles}
           highlightOnHover
-          // fixedHeader
           pointerOnHover
           pagination
           paginationPerPage={5}
@@ -173,3 +80,5 @@ export const Tablev2: React.FC = () => {
     </div>
   );
 };
+
+export default Tablev2;
