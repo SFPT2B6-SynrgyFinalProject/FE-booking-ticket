842278dc3ee02ef0b343972138c3936722498705
arisafriyanto <arisafriyanto1933@gmail.com>
2024-01-29 04:43:01 +0700

    fix: home page & footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,205 +1,64 @@
-
-
-import { Icon } from "@iconify/react/dist/iconify.js";
-import gambarbg from "../assets/images/plane-is-flying-blue-sky.jpg"
-import gambar1 from "../assets/images/famous-borobudur-temple-mungkid-indonesia.webp"
-import gambar2 from "../assets/images/kelingking-beach-sunset-nusa-penida-island-bali-indonesia.webp"
-import gambar3 from "../assets/images/temple-gates-lempuyang-luhur-temple-bali-indonesia.jpg"
-import  { useState } from 'react';
-import Select,  { components, ControlProps, GroupBase } from 'react-select';
-import HeaderSearchItem from "../components/addPerson";
-import { Link } from "react-router-dom";
-
-
-
-
-interface Option {
-  value: string;
-  label: string;
-}
-
-const CustomControl: React.FC<ControlProps<Option, boolean, GroupBase<Option>>> = ({ children, ...props }) => (
-  <components.Control {...props}>
-    
-      <Icon icon="clarity:plane-solid" width={24} className="mr-2"  />
-      {children}
-    
-  </components.Control>
-);
-const CustomControl1: React.FC<ControlProps<Option, boolean, GroupBase<Option>>> = ({ children, ...props }) => (
-  <components.Control {...props}>
-    
-      <Icon icon="bxs:plane-land" width={24} className="mr-2"  />
-      {children}
-    
-  </components.Control>
-);
-
-const CustomControl2: React.FC<ControlProps<Option, boolean, GroupBase<Option>>> = ({ children, ...props }) => (
-  <components.Control {...props}>
-    
-      <Icon icon="solar:chair-2-bold" width={24} className="mr-2"  />
-      {children}
-    
-  </components.Control>
-);
-
-
-
-
-const options = [
-  { value: 'jakarta', label: 'jakarta',  },
-  { value: 'Bali', label: 'Bali' },
-  { value: 'Jogjakarta', label: 'Jogjakarta' },
-];
-const options2 = [
-  { value: 'ekonomi', label: 'ekonomi',  },
-  { value: 'bussines', label: 'bussines' },
-  { value: 'executive', label: 'executive' },
-];
-
+import gambarbg from "../assets/images/plane-is-flying-blue-sky.jpg";
+import gambar1 from "../assets/images/famous-borobudur-temple-mungkid-indonesia.webp";
+import gambar2 from "../assets/images/kelingking-beach-sunset-nusa-penida-island-bali-indonesia.webp";
+import gambar3 from "../assets/images/temple-gates-lempuyang-luhur-temple-bali-indonesia.jpg";
+import SearchBox from "../components/SearchBox";
 
 export default function DumyHome() {
-    const [selectedOption] = useState(null);
-    const [isFocused, setIsFocused] = useState(false);
-
-   
-
-    return (
-        <div>
-            <header className="">
-                <div
-                    className="w-full h-[400px] bg-center bg-cover "
-                    style={{
-                        backgroundImage:
-                            `url(${gambarbg})`,
-                    }}
-                >
-                    <div className="flex  justify-center w-full h-full bg-gray-900 bg-opacity-50 py-12">
-                        <div className="text-center">
-                            <div className="container px-4 mx-auto">
-                                <div className=" mx-auto text-center">
-                                    <p className="mt-8 text-white text-center font-outfit text-4xl leading-10 tracking-tight text-gray-900">
-                                        Let's Start Booking Your Tickets and Soar to Your Dream Destinations!
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </header>
-            <div
-                className="block w-[80%] mx-auto mt-[-100px] rounded-lg bg-white text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] ">
-                <div className="p-6">
-                    <div className="flex">
-                        <button className="flex items-center text-sm bg-primary-normal px-8 py-2 rounded font-medium text-white ml-auto ">
-                            <Icon icon="akar-icons:search" width={16} className="mr-2" />
-                            <Link to={"/search"}>Search</Link>
-                        </button>
-
-                    </div>
-
-                    <div className="flex grid md:grid-cols-3 sm:grid-cols-1 md:gap-10 sm:gap-5 mt-5 pt-5">
-                        <Select
-                            placeholder="from"
-                            components={{ Control: CustomControl }}
-                            classNames={{
-                                control: () => "!bg-white !border !border-gray-300 !text-gray-900 !text-sm !rounded !focus:ring-blue-500 !focus:border-blue-500 !w-full px-2.5 py-0.5",
-                            }}
-                            defaultValue={selectedOption}
-                            options={options}
-                        />
-                        <Select
-                            placeholder="To"
-                            components={{ Control: CustomControl1 }}
-                            classNames={{
-                                control: () => "!bg-white !border !border-gray-300 !text-gray-900 !text-sm !rounded !focus:ring-blue-500 !focus:border-blue-500 !w-full px-2.5 py-0.5",
-                            }}
-                            defaultValue={selectedOption}
-                            options={options}
-                        />
-                        
-                        <HeaderSearchItem />
-                        
-                        
-                       
-                    </div>
-                    <div className="flex grid md:grid-cols-3 sm:grid-cols-1 md:gap-10 sm:gap-5 mt-5 pt-5 ">
-                        <div className="relative">
-                            {!isFocused && <Icon icon="fluent:calendar-20-filled" style={{ position: 'absolute', top: '50%', left: '10px', transform: 'translateY(-50%)', color: '#495057' }} />}
-                            <input
-                                placeholder="Departure Date"
-                                className="bg-white border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pl-10" // Add pl-10 for left padding
-                                type="text"
-                                onFocus={(e) => (e.target.type = 'date', setIsFocused(true))}
-                                onBlur={(e) => (e.target.type = 'text', setIsFocused(false))}
-                                id="date"
-                            />
-                        </div>
-                        <div className="relative">
-                            {!isFocused && <Icon icon="fluent:calendar-20-filled" style={{ position: 'absolute', top: '50%', left: '10px', transform: 'translateY(-50%)', color: '#495057' }} />}
-                            <input
-                                placeholder="Departure Arrival"
-                                className="bg-white border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pl-10" // Add pl-10 for left padding
-                                type="text"
-                                onFocus={(e) => (e.target.type = 'date', setIsFocused(true))}
-                                onBlur={(e) => (e.target.type = 'text', setIsFocused(false))}
-                                id="date"
-                            />
-                        </div>
-                        
-                        <Select
-                            components={{ Control: CustomControl2 }}
-                            classNames={{
-                                control: () => "!bg-white !border !border-gray-300 !text-gray-900 !text-sm !rounded !focus:ring-blue-500 !focus:border-blue-500 !w-full px-2.5 py-0.5",
-                            }}
-                            placeholder="class"
-                            defaultValue={selectedOption}
-                            options={options2}
-                        />
-                        
-
-                       
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="relative">
+        <div
+          className="w-full h-[25rem] bg-center bg-cover"
+          style={{
+            backgroundImage: `url(${gambarbg})`,
+          }}
+        >
+          <div className="flex justify-center w-full h-full bg-gray-800/40">
+            <div className="container text-center">
+              <p className="pt-28 text-white text-center font-outfit text-4xl leading-10 tracking-tight">
+                Let's Start Booking Your Tickets and Soar to Your Dream Destinations!
+              </p>
             </div>
-         
-            <div className="container mx-auto mt-20 mb-20 ">
-                <div className="flex justify-center font-Outfit text-2xl font-semibold leading-10 tracking-wide text-left">
-                    Top Destinations
-                </div>
-
-                <div className="flex mt-10 justify-center">
-                    <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-20">
-                        <div className=" overflow-hidden">
-                            <img
-                                src={gambar1}
-                                alt="Gambar 1"
-                                className="object-cover rounded-lg w-72 h-72 top-36 left-10 shadow-lg"
-                                
-                            />
-
-                        </div>
-                        <div className=" overflow-hidden">
-                            <img
-                                src={gambar2}
-                                alt={gambar2}
-                                 className="object-cover rounded-lg w-72 h-72 top-36 left-10 shadow-lg"
-                            />
-                        </div>
-                        <div className="w-277 h-277 overflow-hidden">
-                            <img
-                                src={gambar3}
-                                alt="Gambar 1"
-                                 className="object-cover rounded-lg w-72 h-72 top-36 left-10 shadow-lg"
-                            />
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
-           
+          </div>
         </div>
-    );
+
+        <div className="absolute -bottom-48 md:-bottom-28 left-0 right-0">
+          <SearchBox />
+        </div>
+      </div>
+
+      <div className="container mt-64 sm:mt-48 sm:mb-16 lg:mb-24">
+        <div className="flex justify-center">
+          <h1 className="font-outfit text-3xl font-medium leading-10 tracking-wide">
+            Top Destinations
+          </h1>
+        </div>
+
+        <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-x-28 mt-20 place-items-center">
+          <div className="w-[18rem] h-[17.4rem] mb-10 lg:mb-0">
+            <img
+              src={gambar3}
+              alt="Top 1"
+              className="h-full object-cover object-center rounded-3xl shadow-xl"
+            />
+          </div>
+          <div className="w-[18rem] h-[17.4rem] mb-10 lg:mb-0">
+            <img
+              src={gambar2}
+              alt="Top 2"
+              className="h-full object-cover object-center rounded-3xl shadow-xl"
+            />
+          </div>
+          <div className="w-[18rem] h-[17.4rem] mb-10 lg:mb-0">
+            <img
+              src={gambar1}
+              alt="Top 3"
+              className="h-full object-cover object-center rounded-3xl shadow-xl"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
