0c6044eaaf90f5502a238fd7fdf91a7a04a3920c
agung749 <agungciccone749@gmail.com>
2024-02-03 12:13:48 +0700

    oke bray

--- src/containers/buyers/profiles/list/list.hooks.tsx
+++ src/containers/buyers/profiles/list/list.hooks.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useLocation } from "react-router";
+import { NavigateFunction, useLocation, useNavigate } from "react-router";
 import { RootState } from "../../../../config/redux/store";
 
 export default function useList() {
   const location = useLocation();
   const [active, setActive] = useState<number>(1);
+  const [logOut, setLogOut] = useState<boolean>(false);
+  const [berhasilLogOut, setBerhasilLogOut] = useState<boolean>(false);
+  const navigate: NavigateFunction = useNavigate();
+
   const profiles = useSelector((state: RootState) => state.userReducer);
 
   // Update active value based on the current route
@@ -16,9 +20,33 @@ export default function useList() {
       setActive(1);
     }
   }, [location.pathname]);
+  const handleLogOut = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    const { id } = event.currentTarget as HTMLAnchorElement; // Use currentTarget instead of target
 
+    if (id === 'batal') {
+      setLogOut(false);
+    } else {
+      setLogOut(true);
+    }
+  };
+  
+  const setPurge=():void=>{
+      localStorage.clear();
+      setBerhasilLogOut(true)
+      setLogOut(false)
+  }
+  const handleRedirect=():void=>{
+    navigate("/");
+    window.location.reload();
+  }
   return {
     active,
     profiles,
+    berhasilLogOut,
+    setPurge,
+    handleLogOut,
+    handleRedirect,
+    logOut, 
+    close
   };
 }
