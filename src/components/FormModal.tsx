e30496e532ce02aee0cfaf144b09b842d556db01
maddhdyt <cemilcemil040704@gmail.com>
2024-02-06 17:00:42 +0700

    first pull request

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -36,15 +36,18 @@ export const FormModal: React.FC<FormModalProps> = ({ title, isOpen, children })
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium mb-5 leading-6 text-gray-900"
-                  >
-                    {title}
-                  </Dialog.Title>
-
-                  {children}
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
+                  <div className="bg-primary-normal px-6 py-4">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-white font-sans"
+                    >
+                      {title}
+                    </Dialog.Title>
+                  </div>
+                  <div className="px-6 py-4 pb-8">
+                    {children}
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
