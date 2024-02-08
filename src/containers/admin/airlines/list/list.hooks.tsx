244abbe457c4e5fa1995d12bf0c22b32d73778aa
arisafriyanto <arisafriyanto1933@gmail.com>
2024-02-08 15:04:31 +0700

    fix: add and edit airlines

--- src/containers/admin/airlines/list/list.hooks.tsx
+++ src/containers/admin/airlines/list/list.hooks.tsx
@@ -1,21 +1,21 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { IAirLines } from "../airlines.types";
 import { fetchInstance } from "../../../../lib/services/core";
 import { useUserToken } from "../../../../lib/services/auth";
 
-
-
 export default function useList() {
   const [open, setOpen] = useState<boolean>(false);
   const [records, setRecords] = useState<IAirLines[]>([]);
-  const [maskapai, setBandaraAsal] = useState("");
-  const [kode, setBandaraTujuan] = useState("");
-  const [judul, setJudul] = useState("Tambah Data maskapai");
-  const [deleteId, setDeleteId] = useState("");
-  const [data, setData] = useState<IAirLines[]>([]);
+  const [judul, setJudul] = useState("");
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [showAddForm, setShowAddForm] = useState(false);
+  const [originalRecords, setOriginalRecords] = useState<IAirLines[]>([]);
+
+  const [formValues, setFormValues] = useState<IAirLines>({
+    id: 0,
+    name: "",
+    code: "",
+  });
 
   const fetchData = async () => {
     try {
@@ -25,86 +25,120 @@ export default function useList() {
         endpoint: "/api/admin/airlines?dataPerPage=50&page=1",
         method: "GET",
       });
-      setData(response.data["airlines"]);
-      setRecords(response.data["airlines"]); // assuming you want to set records with the fetched data
+
+      const airlines = response.data.airlines;
+      const sortedAirlines = airlines.sort((a: IAirLines, b: IAirLines) => b.id - a.id);
+
+      setRecords(sortedAirlines);
+      setOriginalRecords(airlines);
     } catch (error) {
       console.log("error:", error);
     } finally {
       setIsLoading(false);
     }
-
   };
 
   const clickOpen = (): void => {
     setJudul("Tambah Data maskapai");
     setOpen(true);
-    setShowAddForm(false); 
+    setShowAddForm(false);
   };
 
   const clickClose = (): void => {
-    setShowAddForm(false); 
-    setBandaraAsal(""); // Reset nilai isian form ke nilai awal
-    setBandaraTujuan(""); 
+    setShowAddForm(false);
+    setFormValues({ ...formValues, id: 0, name: "", code: "" });
     setOpen(false);
   };
 
-  const postData = async () => {
-  try {
-    setIsLoading(true);
-    const response = await fetchInstance({
-      authToken: useUserToken(),
-      endpoint: "/api/admin/airlines",
-      method: "POST"
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchValue = e.target.value.toLocaleLowerCase();
+    const newData = originalRecords.filter((row: { name: string; code: string }) => {
+      return (
+        row.name.toLowerCase().includes(searchValue) || row.code.toLowerCase().includes(searchValue)
+      );
     });
-    console.log("Data berhasil ditambahkan:", response);
-    // Setelah berhasil menambahkan data, Anda mungkin ingin melakukan pengambilan data ulang untuk memperbarui records
-    fetchData();
-  } catch (error) {
-    console.log("Error menambahkan data:", error);
-  } finally {
-    setIsLoading(false);
-    setOpen(false); // Tutup modal setelah menambahkan data
-  }
-};
 
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setJudul("Tambah Data maskapai");
-    const { name, value } = event.target;
-    switch (name) {
-      case "bandara-asal":
-        setBandaraAsal(value);
-        break;
-      case "bandara-tujuan":
-        setBandaraTujuan(value);
-        break;
-    }
+    setRecords(newData);
   };
 
   const handleEdit = (id: number): void => {
     setJudul("Ubah Data maskapai");
-    setBandaraAsal(data[id - 1]["name"]);
-    setBandaraTujuan(data[id - 1]["code"]);
-    setOpen(true);
-    setShowAddForm(false); 
-  };
+    const airline = records.find((airline) => airline.id === id);
+    if (airline) {
+      const { id, name, code } = airline;
+      setFormValues({ ...formValues, id, name, code });
+    }
 
-  const handleDelete = (id: any): void => {
-    setDeleteId(id);
-    setJudul("Hapus Data maskapai");
     setOpen(true);
     setShowAddForm(false);
   };
 
-  const handleFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newData = data.filter((row: { name: string; code: string }) => {
-      return (
-        row.name.toLowerCase().includes(e.target.value.toLowerCase()) ||
-        row.code.toLowerCase().includes(e.target.value.toLowerCase())
-      );
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
     });
-    setRecords(newData);
   };
+
+  const postData = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const payload: IAirLines = { ...formValues };
+
+      const response = await fetchInstance({
+        endpoint: "/api/admin/airlines",
+        method: "POST",
+        authToken: useUserToken(),
+        data: payload,
+      });
+
+      // definisikan message
+      console.log(response.message);
+
+      fetchData();
+    } catch (error) {
+      console.log("Error menambahkan data:", error);
+    } finally {
+      setIsLoading(false);
+      setOpen(false);
+    }
+  };
+
+  const editData = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const payload: IAirLines = { ...formValues };
+
+      const response = await fetchInstance({
+        endpoint: "/api/admin/airlines",
+        method: "PUT",
+        authToken: useUserToken(),
+        data: payload,
+      });
+
+      // definisikan message
+      console.log(response.message);
+
+      fetchData();
+    } catch (error) {
+      console.log("Error edit data:", error);
+    } finally {
+      setIsLoading(false);
+      setOpen(false);
+    }
+  };
+
+  // const handleDelete = (id: any): void => {
+  //   setDeleteId(id);
+  //   setJudul("Hapus Data maskapai");
+  //   setOpen(true);
+  //   setShowAddForm(false);
+  // };
 
   useEffect(() => {
     fetchData();
@@ -112,20 +146,18 @@ export default function useList() {
 
   return {
     postData,
+    editData,
     showAddForm,
     isLoading,
     records,
     open,
     clickOpen,
     clickClose,
-    maskapai,
-    kode,
     judul,
-    deleteId,
     handleEdit,
-    handleDelete,
-    handleFilter,
+    // handleDelete,
+    handleSearch,
     handleChange,
+    formValues,
   };
 }
-
--- src/containers/admin/airlines/list/list.tsx
+++ src/containers/admin/airlines/list/list.tsx
@@ -35,31 +35,26 @@ const customStyles = {
 
 const AirLine: React.FC<TableProps> = () => {
   const {
-    
     isLoading,
     records,
     open,
     clickOpen,
     clickClose,
-    maskapai,
-    kode,
     judul,
-    deleteId,
     handleEdit,
-    handleDelete,
-    handleFilter,
+    // handleDelete,
+    handleSearch,
     handleChange,
+    postData,
+    editData,
+    formValues,
   } = useAction();
-  
-
-
-
 
   const columns = [
     {
       name: "Maskapai",
       selector: (row: IAirLines) => row.name,
-      width: "35rem",
+      width: "28rem",
       sortable: true,
     },
     {
@@ -83,13 +78,13 @@ const AirLine: React.FC<TableProps> = () => {
           >
             Edit <Icon icon="bx:edit" width={18} />
           </Button>
-          <Button
+          {/* <Button
             onClick={() => handleDelete(row.id)}
             className={`text-white bg-rose-600 !px-4`}
             size="xs"
           >
             Hapus <Icon icon="ci:trash-empty" width={19} />
-          </Button>
+          </Button> */}
         </div>
       ),
     },
@@ -98,50 +93,62 @@ const AirLine: React.FC<TableProps> = () => {
   return (
     <div className="flex-1 px-4 mt-8 md:px-8 md:mt-14">
       <FormModal isOpen={open} title={judul}>
-        {judul === "Ubah Data maskapai" || judul === "Tambah Data maskapai" ? (
-          <div className="mt-2">
-            <div className="mb-3">
-              <InputComponent
-                placeholder="Maskapai "
-                type="text"
-                id="bandara-asal"
-                name="bandara-asal"
-                value={maskapai}
-                onChange={handleChange}
-              />
+        {/* Ternary 3 kondisi */}
+
+        {/* <form onSubmit={
+              judul === "Tambah Data maskapai"
+              ? postData
+              : judul === "Edit Data Maskapai"
+              ? editData
+              : deleteData
+          } > */}
+
+        <form onSubmit={judul === "Tambah Data maskapai" ? postData : editData}>
+          {judul === "Tambah Data maskapai" || judul === "Ubah Data maskapai" ? (
+            <div className="mt-2">
+              <div className="mb-3">
+                <InputComponent
+                  placeholder="Maskapai "
+                  type="text"
+                  id="name"
+                  name="name"
+                  onChange={handleChange}
+                  value={formValues.name}
+                  required
+                />
+              </div>
+              <div>
+                <InputComponent
+                  placeholder="Kode"
+                  type="text"
+                  id="code"
+                  name="code"
+                  onChange={handleChange}
+                  value={formValues.code}
+                  required
+                />
+              </div>
             </div>
-            <div>
-              <InputComponent
-                placeholder="Kode"
-                type="text"
-                id="bandara-tujuan"
-                name="bandara-tujuan"
-                value={kode}
-                onChange={handleChange}
-              />
-            </div>
+          ) : (
+            <p>Anda Yakin Menghapus Data Ini?</p>
+          )}
+
+          <div className="mt-4 flex gap-x-4 justify-center">
+            <button
+              type="button"
+              className="bg-white text-primary-normal border border-gray-300 hover:bg-gray-100 rounded-xl text-sm font-medium py-[8px] px-14 w-fit"
+              onClick={clickClose}
+            >
+              Close
+            </button>
+
+            <Button type="primary-dark" className=" rounded-xl" color="primary-dark" size="sm">
+              Simpan
+            </Button>
           </div>
-        ) : (
-          <p>Anda Yakin Menghapus Data Ini?</p>
-        )}
-
-        <div className="mt-4 flex gap-x-4 justify-center">
-          <input type="hidden" id="hiddenInput" name="hiddenInput" value={deleteId} />
-          <Button
-            type="secondary"
-            className="border border-gray-300 hover:bg-gray-100 rounded-xl"
-            color="secondary-normal"
-            size="sm"
-            onClick={clickClose}
-          >
-            Close
-          </Button>
-
-          <Button type="primary-dark" className=" rounded-xl" color="primary-dark" size="sm">
-            {judul === "Hapus Data maskapai" ? "Hapus" : "Simpan"}
-          </Button>
-        </div>
+        </form>
       </FormModal>
+
       <div className="flex flex-col md:flex-row md:justify-between items-center">
         <Button
           className={`!bg-green-600 py-[11px] text-white px-[14px] sm:px-[35px] md:px-[24px]`}
@@ -155,12 +162,11 @@ const AirLine: React.FC<TableProps> = () => {
             type="text"
             placeholder="Cari..."
             customStyle="border border-gray-400/80 px-4 py-[10px] rounded-xl"
-            onChange={handleFilter}
+            onChange={handleSearch}
           />
         </div>
       </div>
       <Tablev2 isPending={isLoading} columns={columns} data={records} customStyles={customStyles} />
-
     </div>
   );
 };
