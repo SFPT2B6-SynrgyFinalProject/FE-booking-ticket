390cbb595cf79de94dc42902ce16f6c7c0a65c1d
maddhdyt <cemilcemil040704@gmail.com>
2024-02-12 10:09:51 +0700

    switch device

--- src/containers/admin/users/list/list.hooks.tsx
+++ src/containers/admin/users/list/list.hooks.tsx
@@ -1,149 +1,216 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { IUsers } from "../users.types";
+import { fetchInstance } from "../../../../lib/services/core";
+import { useUserToken } from "../../../../lib/services/auth";
+import { AlertProps } from "../../../../components/Alert";
 
 export default function useList() {
+  const [open, setOpen] = useState<boolean>(false);
   const [records, setRecords] = useState<IUsers[]>([]);
+  const [judul, setJudul] = useState("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [showAddForm, setShowAddForm] = useState(false);
+  const [originalRecords, setOriginalRecords] = useState<IUsers[]>([]);
+  const [alert, setAlert] = useState<AlertProps | null>(null);
 
+  const [formValues, setFormValues] = useState<IUsers>({
+    id: 0,
+    name: "",
+    nohp: "",
+    email: "",
+    role: "",
+  });
+  useEffect(() => {
+    if (alert !== null) {
+      const timeoutId = setTimeout(() => {
+        setAlert(null);
+      }, 2000);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [alert]);
   // data from api
-  const data = [
-    {
-      id: 1,
-      name: "John Doe",
-      nohp: "1234567890",
-      email: "john.doe@example.com",
-      role: "Admin",
-    },
-    {
-      id: 2,
-      name: "Jane Michel",
-      nohp: "9876543210",
-      email: "jane.mi@example.com",
-      role: "Buyer",
-    },
-    {
-      id: 3,
-      name: "Alice Smith",
-      nohp: "5551234567",
-      email: "alice.smith@example.com",
-      role: "Buyer",
-    },
-    {
-      id: 4,
-      name: "Bob Johnson",
-      nohp: "8885551234",
-      email: "bob.johnson@example.com",
-      role: "Buyer",
-    },
-    {
-      id: 5,
-      name: "Eve Wilson",
-      nohp: "7778889999",
-      email: "eve.wilson@example.com",
-      role: "Buyer",
-    },
-    {
-      id: 6,
-      name: "Charlie Brown",
-      nohp: "5554443333",
-      email: "charlie.brown@example.com",
-      role: "Admin",
-    },
-    {
-      id: 7,
-      name: "David Smith",
-      nohp: "1112223333",
-      email: "david.smith@example.com",
-      role: "Buyer",
-    },
-    {
-      id: 8,
-      name: "Grace Davis",
-      nohp: "4445556666",
-      email: "grace.davis@example.com",
-      role: "Buyer",
-    },
-    {
-      id: 9,
-      name: "Frank Johnson",
-      nohp: "9998887777",
-      email: "frank.johnson@example.com",
-      role: "Admin",
-    },
-    {
-      id: 10,
-      name: "Helen Brown",
-      nohp: "7776665555",
-      email: "helen.brown@example.com",
-      role: "Buyer",
-    },
-    {
-      id: 11,
-      name: "Ivy Wilson",
-      nohp: "3332221111",
-      email: "ivy.wilson@example.com",
-      role: "Admin",
-    },
-    {
-      id: 12,
-      name: "Jack Smith",
-      nohp: "6667778888",
-      email: "jack.smith@example.com",
-      role: "Buyer",
-    },
-    {
-      id: 13,
-      name: "Karen Davis",
-      nohp: "2223334444",
-      email: "karen.davis@example.com",
-      role: "Buyer",
-    },
-    {
-      id: 14,
-      name: "Leo Johnson",
-      nohp: "7779998888",
-      email: "leo.johnson@example.com",
-      role: "Admin",
-    },
-    {
-      id: 15,
-      name: "Mia Brown",
-      nohp: "8887776666",
-      email: "mia.brown@example.com",
-      role: "Buyer",
-    },
-  ];
+
 
   const fetchUsers = async () => {
     try {
-      // logic fetch data
-      setRecords(data);
+      setIsLoading(true);
+      const response = await fetchInstance({
+        authToken: useUserToken(),
+        endpoint: "/api/admin/users?dataPerPage=50&page=1",
+        method: "GET",
+      });
+
+      const users = response.data.users;
+      const sortedUsers = users.sort((a: IUsers, b: IUsers) => b.id - a.id);
+
+      setRecords(sortedUsers);
+      setOriginalRecords(users);
     } catch (error) {
-      console.log("error");
+      console.log("error:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  const handleFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newData = data.filter((row: { name: string; role: string }) => {
+  const clickOpen = (): void => {
+    setJudul("Tambah Data User");
+    setOpen(true);
+    setShowAddForm(false);
+  };
+
+  const clickClose = (): void => {
+    setShowAddForm(false);
+    setFormValues({ ...formValues, id: 0, name: "", nohp: "", email: "", role: "" });
+    setOpen(false);
+  };
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchValue = e.target.value.toLowerCase();
+    const newData = originalRecords.filter((row: { name?: string; nohp?: string; email?: string; role?: string;}) => {
       return (
-        row.name.toLowerCase().includes(e.target.value.toLowerCase()) ||
-        row.role.toLowerCase().includes(e.target.value.toLowerCase())
+        (row.name && row.name.toLowerCase().includes(searchValue)) ||
+        (row.nohp && row.nohp.toLowerCase().includes(searchValue)) ||
+        (row.email && row.email.toLowerCase().includes(searchValue))
       );
     });
+
     setRecords(newData);
+};
+
+  const handleEdit = (id: number): void => {
+    setJudul("Ubah Data User");
+    const users = records.find((users) => users.id === id);
+    if (users) {
+      const { id, name, nohp, email, role } = users;
+      setFormValues({ ...formValues, id, name, nohp, email, role });
+    }
+
+    setOpen(true);
+    setShowAddForm(false);
   };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const postData = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const payload: IUsers = { ...formValues };
+
+      const response = await fetchInstance({
+        endpoint: "/api/admin/users",
+        method: "POST",
+        authToken: useUserToken(),
+        data: payload,
+      });
+
+      // definisikan message
+      console.log(response.message);
+      setAlert({
+        type: "success",
+        message: "Data user berhasil ditambahkan!",
+      });
+      setFormValues({ ...formValues, id: 0, name: "", nohp: "", email: "", role: "" });
+      
+      fetchUsers();
+    } catch (error) {
+      console.log("Error menambahkan data:", error);
+      setAlert({
+        type: "fail",
+        message: "Data user gagal ditambahkan!",
+      });
+    } finally {
+      setIsLoading(false);
+      setOpen(false);
+    }
+  };
+
+  const editData = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+  
+    try {
+      const payload: IUsers = { ...formValues };
+  
+      const response = await fetchInstance({
+        endpoint: `/api/admin/users/${formValues.id}`, // Include the id in the endpoint
+        method: "PUT",
+        authToken: useUserToken(),
+        data: payload,
+      });
+  
+      // definisikan message
+      console.log(response.message);
+      setAlert({
+        type: "success",
+        message: "Data user berhasil diubah!",
+      });
+      setFormValues({ ...formValues, id: 0, name: "", nohp: "", email: "", role: "" });
+  
+      fetchUsers();
+    } catch (error) {
+      console.log("Error edit data:", error);
+      setAlert({
+        type: "fail",
+        message: "Data user gagal diubah!",
+      });
+    } finally {
+      setIsLoading(false);
+      setOpen(false);
+    }
+  };
+  
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
-  const handleEdit = (id: number): void => {
-    console.log(`Editing record with ID ${id}`);
+  const handleDelete = async (id: number): Promise<void> => {
+    try {
+      await fetchInstance({
+        endpoint: `/api/admin/users/${id}`,
+        method: "DELETE",
+        authToken: useUserToken(),
+      });
+
+      setAlert({
+        type: "success",
+        message: "Data user berhasil dihapus!",
+      });
+
+      fetchUsers();
+    } catch (error) {
+      console.log("Error menghapus data:", error);
+      setAlert({
+        type: "fail",
+        message: "Data user gagal dihapus!",
+      });
+    }
   };
 
-  const handleDelete = (id: number): void => {
-    console.log(`Deleting record with ID ${id}`);
+  return {
+    alert,
+    postData,
+    editData,
+    showAddForm,
+    isLoading,
+    records,
+    open,
+    clickOpen,
+    clickClose,
+    judul,
+    handleEdit,
+    handleDelete,
+    handleSearch,
+    handleChange,
+    formValues,
   };
-
-  return { records, handleEdit, handleDelete, handleFilter };
 }
--- src/containers/admin/users/list/list.tsx
+++ src/containers/admin/users/list/list.tsx
@@ -4,7 +4,9 @@ import Button from "../../../../components/Button";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { IUsers } from "../users.types";
 import InputComponent from "../../../../components/Input";
+import { FormModal } from "../../../../components/FormModal";
 import useAction from "./list.hooks";
+import Alert from "../../../../components/Alert";
 
 interface TableColumn {
   name: string;
@@ -33,7 +35,22 @@ const customStyles = {
 };
 
 const List: React.FC<TableProps> = () => {
-  const { records, handleEdit, handleDelete, handleFilter } = useAction();
+  const { 
+    alert,
+    isLoading,
+    records,
+    open,
+    clickOpen,
+    clickClose,
+    judul,
+    handleEdit,
+    handleDelete,
+    handleSearch,
+    handleChange,
+    postData,
+    editData,
+    formValues,
+  } = useAction();
 
   const columns = [
     {
@@ -86,24 +103,123 @@ const List: React.FC<TableProps> = () => {
   ];
 
   return (
+    // <div className="flex-1 px-4 mt-8 md:px-8 md:mt-14">
+    //   <div className="flex flex-col md:flex-row md:justify-between items-center">
+    //     <Button
+    //       className={`!bg-green-600 py-[11px] text-white px-[14px] sm:px-[35px] md:px-[24px]`}
+    //       size="xs"
+    //     >
+    //       Tambah Pengguna <Icon icon="prime:file-import" width={20} />
+    //     </Button>
+    //     <div className="mt-4 w-full sm:w-fit">
+    //       <InputComponent
+    //         type="text"
+    //         placeholder="Cari..."
+    //         customStyle="border border-gray-400/80 px-4 py-[10px] rounded-xl"
+    //         onChange={handleFilter}
+    //       />
+    //     </div>
+    //   </div>
+    //   <Tablev2 columns={columns} data={records} customStyles={customStyles} />
+    // </div>
     <div className="flex-1 px-4 mt-8 md:px-8 md:mt-14">
-      <div className="flex flex-col md:flex-row md:justify-between items-center">
-        <Button
-          className={`!bg-green-600 py-[11px] text-white px-[14px] sm:px-[35px] md:px-[24px]`}
-          size="xs"
-        >
-          Tambah Pengguna <Icon icon="prime:file-import" width={20} />
-        </Button>
-        <div className="mt-4 w-full sm:w-fit">
+       {alert && (
+          <Alert type={alert.type} message={alert.message} />// Display the alert if it exists
+      )}
+      
+      <FormModal isOpen={open} title={judul}>
+        {/* Ternary 3 kondisi */}
+
+        {/* <form onSubmit={
+              judul === "Tambah Data maskapai"
+              ? postData
+              : judul === "Edit Data Maskapai"
+              ? editData
+              : deleteData
+          } > */}
+
+        <form onSubmit={judul === "Tambah Data User" ? postData : editData}>
+          {judul === "Tambah Data User" || judul === "Ubah Data User" ? (
+            <div className="mt-2">
+              <div className="mb-3">
+                <InputComponent
+                  placeholder="Name"
+                  type="text"
+                  id="name"
+                  name="name"
+                  onChange={handleChange}
+                  value={formValues.name}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <InputComponent
+                  placeholder="No Telepon"
+                  type="text"
+                  id="nohp"
+                  name="nohp"
+                  onChange={handleChange}
+                  value={formValues.nohp}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <InputComponent
+                  placeholder="Email"
+                  type="text"
+                  id="email"
+                  name="email"
+                  onChange={handleChange}
+                  value={formValues.email}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <InputComponent
+                  placeholder="Role"
+                  type="text"
+                  id="role"
+                  name="role"
+                  onChange={handleChange}
+                  value={formValues.role}
+                  required
+                />
+              </div>
+            </div>
+          ) : (
+            <p>Anda Yakin Menghapus Data Ini?</p>
+          )}
+
+          <div className="mt-4 flex gap-x-4 justify-center">
+            <button
+              type="button"
+              className="bg-white text-primary-normal border border-gray-300 hover:bg-gray-100 rounded-xl text-sm font-medium py-[8px] px-14 w-fit"
+              onClick={clickClose}
+            >
+              Close
+            </button>
+
+            <Button type="primary-dark" className=" rounded-xl" color="primary-dark" size="sm">
+              Simpan
+            </Button>
+          </div>
+        </form>
+      </FormModal>
+
+      <div className="flex flex-col md:flex-row md:justify-between items-center border-t border-b border-[#000] border-opacity-15 py-[15px] px-3">
+        <button className="bg-green-600 py-2 px-3 rounded-[10px] text-white font-outfit flex items-center gap-2" onClick={clickOpen}>
+          Tambah User <Icon icon="prime:file-import" width={20} />
+        </button>
+        <div className="w-full sm:w-fit">
           <InputComponent
             type="text"
             placeholder="Cari..."
-            customStyle="border border-gray-400/80 px-4 py-[10px] rounded-xl"
-            onChange={handleFilter}
+            customStyle="border border-[#000] border-opacity-25 px-4 py-[10px] rounded-[10px] text-base"
+            onChange={handleSearch}
           />
         </div>
       </div>
-      <Tablev2 columns={columns} data={records} customStyles={customStyles} />
+      <Tablev2 isPending={isLoading} columns={columns} data={records} customStyles={customStyles} />
     </div>
   );
 };
